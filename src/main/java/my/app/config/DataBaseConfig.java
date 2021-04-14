c00b7ab399fa7ebaeb1c0c35aa359af11a014064
mr-nicko89 <mr-nicko89@yandex.ru>
2021-04-14 18:35:16 +0300

    Подключил EntityManager. Требуется -перенести настройки в файл с настройками - сделать нормальный сервисный слой - подключить DAO Hibernate

---
+++ src/main/java/my/app/config/DataBaseConfig.java
@@ -0,0 +1,55 @@
+package my.app.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import javax.sql.DataSource;
+
+
+
+@Configuration
+@EnableTransactionManagement
+@ComponentScan(basePackages = "my.app.config")
+//@EnableJpaRepositories("my.app.config")
+public class DataBaseConfig {
+
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/spring_peoplebase?verifyServerCertificate=false&useSSL=false&requireSSL=false&useLegacyDatetimeCode=false&amp&serverTimezone=UTC");
+        dataSource.setUsername("root1");
+        dataSource.setPassword("12345_Aa");
+        return dataSource;
+    }
+
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(true);
+        vendorAdapter.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect");
+        vendorAdapter.setShowSql(true);
+        LocalContainerEntityManagerFactoryBean factory =
+                new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setDataSource(dataSource());
+        factory.setPackagesToScan("my.app.config");
+        return factory;
+    }
+
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(entityManagerFactory().getObject());
+        return txManager;
+    }
+}
--- src/main/java/my/app/models/Person.java
+++ src/main/java/my/app/models/Person.java
@@ -1,5 +1,6 @@
 package my.app.models;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -10,20 +11,23 @@ import javax.validation.constraints.Size;
 
 
 @Entity
-@Table(name = "PEOPLE")
+@Table(name = "people")
 public class Person {
     @Id
     private int id;
 
     @NotEmpty(message = "Name should not be empty")
     @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
+    @Column(name = "name")
     private String name;
 
     @Min(value = 0, message = "Age should be greater than 0")
+    @Column(name = "age")
     private int age;
 
     @NotEmpty(message = "Email should not be empty")
     @Email(message = "Email should be valid")
+    @Column(name = "email")
     private String email;
 
     public Person() {
